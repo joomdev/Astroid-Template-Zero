62a88d804bbc5c3cbd488cc58c3e9e1e3755c7b2
Hitesh Aggarwal <hitesh99raj@gmail.com>
2020-08-31 22:39:43 +0530

    Zero Updates

--- html/mod_articles_latest/media.php
+++ html/mod_articles_latest/media.php
@@ -9,12 +9,12 @@
 defined('_JEXEC') or die;
 ?>
 <div class="latestnews view-media">
-   <ul class="list-group list-group-flush">
+   <ul class="list-group">
       <?php foreach ($list as $item) : $image = json_decode($item->images); ?>
-      <li itemscope itemtype="https://schema.org/Article" class="list-group-item px-0">
+      <li itemscope itemtype="https://schema.org/Article">
          <?php if($image->image_intro != "") : ?>
             <a class="article-media" href="<?php echo $item->link; ?>" itemprop="url">
-               <img class="card-img-top pb-2" src="<?php echo JURI::root().$image->image_intro; ?>" alt="<?php echo htmlspecialchars($image->image_fulltext_alt); ?>">
+               <img src="<?php echo JURI::root().$image->image_intro; ?>" alt="<?php echo htmlspecialchars($image->image_fulltext_alt); ?>">
             </a>
          <?php endif; ?>
          <a class="article-title" href="<?php echo $item->link; ?>" itemprop="url">
